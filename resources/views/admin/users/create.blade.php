8e7c61b86ef71f87a38dadae240e6ebd34391cfe
wilmar051081 <40595538+wilmar051081@users.noreply.github.com>
2019-08-14 16:22:00 +0800

    users page working without photos

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+
+    <h1>Create User</h1>
+
+    <div>@include('includes.form_error')</div>
+
+    {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store', 'file'=>true]) !!}
+
+        <div class="form-group">
+            {!! Form::label('name', 'Name:') !!}
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('email', 'Email:') !!}
+            {!! Form::email('email', null, ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('role_id', 'Role:') !!}
+            {!! Form::select('role_id', [''=>'Choose Options'] + $roles , null, ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+              {!! Form::label('is_active', 'Status:') !!}
+              {!! Form::select('is_active', array(1 =>'Active', 0 => 'Not Active'), 0, ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+              {!! Form::label('file', 'File:') !!}
+              {!! Form::file('file', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+              {!! Form::label('password', 'Password:') !!}
+              {!! Form::password('password', ['class'=>'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+        </div>
+        {!! Form::close() !!}
+
+
+@stop
