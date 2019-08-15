0b39020574d4b94842a77fb34b5b231dbf4e109b
wilmar051081 <40595538+wilmar051081@users.noreply.github.com>
2019-08-15 19:19:45 +0800

    users with uploading feature working

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -27,8 +27,8 @@
               {!! Form::select('is_active', array(1 =>'Active', 0 => 'Not Active'), 0, ['class'=>'form-control']) !!}
         </div>
         <div class="form-group">
-              {!! Form::label('file', 'File:') !!}
-              {!! Form::file('file', null, ['class'=>'form-control']) !!}
+              {!! Form::label('photo_id', 'Photo:') !!}
+              {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
         </div>
 
         <div class="form-group">
